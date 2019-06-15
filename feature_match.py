a5fee73bf96f51b8016abe22273148beaa1cb9aa
Paras Dahiya <paras.d16@iiits.in>
2019-06-15 11:56:49 +0530

    Calculated Hy, fixed issue in feature_match

--- feature_match.py
+++ feature_match.py
@@ -8,31 +8,28 @@ def get_matching_inliers(img_master, img_slave, show_matches = True):
     kp2, des2 = sift.detectAndCompute(img_slave, None)
 
     bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
+    matches = bf.knnMatch(des1, des2, k=1)
 
     print("Initially detected interest points: ", len(matches))
 
-    good = []
-    for m, n in matches:
-        if m.distance < 0.75 * n.distance:
-            good.append(m)
-
-    if len(good) > 10:
-        src_pts = np.float32([kp1[m.queryIdx].pt for m in good])
-        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good])
+    if len(matches) > 10:
+        src_pts = np.float32([kp1[m.queryIdx].pt for [m] in matches])
+        dst_pts = np.float32([kp2[m.trainIdx].pt for [m] in matches])
     F, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
     matchesMask = mask.ravel().tolist()
     inliers = []
-    for i, m in enumerate(good):
+    for i, [m] in enumerate(matches):
         if matchesMask[i] != 0:
             inliers.append([m])
     print("Interest points after RANSAC - Inliers: ", len(inliers))
 
     if show_matches:
-        img3 = cv2.drawMatchesKnn(img_master, kp1, img_slave, kp2, inliers, None, flags=2)
+        img3 = cv2.drawMatchesKnn(img_master, kp1, img_slave, kp2, inliers[9:10], None, flags=2)
         cv2.imshow('Image', img3)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     master_pts = np.float32([kp1[m.queryIdx].pt for [m] in inliers])
-    slave_pts = np.float32([kp2[m.queryIdx].pt for [m] in inliers])
+    slave_pts = np.float32([kp2[m.trainIdx].pt for [m] in inliers])
 
     return master_pts, slave_pts
